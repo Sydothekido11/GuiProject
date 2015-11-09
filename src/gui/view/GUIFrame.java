61b9c6174b02aafc921f3af11d1c2a4f47965d7e
Sydothekido11 <sydnemelka@gmail.com>
2015-11-09 12:37:52 -0700

    Added the helper method in the GUIFrame.

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -4,9 +4,9 @@ import javax.swing.JFrame;
 import gui.controller.GuiController;
 
 /**
- * 
+ * Creates the frame for the GUI project.
  * @author snem8901
- * @version 1.0 11/9/15
+ * @version 1.2 11/9/15 Added the helper method
  * 
  */
 public class GUIFrame extends JFrame
@@ -17,5 +17,12 @@ public class GUIFrame extends JFrame
 	public GUIFrame(GuiController baseController)
 	{
 		this.baseController = baseController; //Assign parameter to data member.
+		setupFrame();
+		
+	}
+	
+	private void setupFrame()
+	{
+		this.setVisible(true);	//Must be the last line of code.
 	}
 }
