35cda11eb24aa0ef6625a9c9077df8c162eda754
Sydothekido11 <sydnemelka@gmail.com>
2015-11-09 13:23:18 -0700

    Added helpers to the panel.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -2,6 +2,9 @@ package gui.view;
 
 import javax.swing.JPanel;
 import gui.controller.GuiController;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 /**
  * 
@@ -12,11 +15,41 @@ public class GUIPanel extends JPanel
 {
 
 	private GuiController baseController;
+	private JButton firstButton;
+	private JTextField firstTextField;
+	private SpringLayout baseLayout;
+	
 	
 	public GUIPanel(GuiController baseController)
 	{
 		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		firstButton = new JButton("Press teh button.");
+		firstTextField = new JTextField("Type your words here.");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
 	
+	/**
+	 * Loads content into the panel.
+	 */
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout); //Needs to be the first line of the setupPanel helper method.
+		this.add(firstButton);
+		this.add(firstTextField);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 	
 }
