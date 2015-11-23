083ffdb145a9ec0cd773b18e393dc23f9d1fcc81
Sydothekido11 <sydnemelka@gmail.com>
2015-11-23 11:59:54 -0700

    Added a change random color method.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import java.awt.Color;
 
 /**
  * 
@@ -96,6 +97,17 @@ public class GUIPanel extends JPanel
 				
 			}
 		});
+	}
+	
+	private void changeRandomColor()
+	{
+		int red, green, blue;
+		
+		red = (int) (Math.random() * 256);
+		green = (int) (Math.random() * 256);
+		blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
 	}
 	
 }
